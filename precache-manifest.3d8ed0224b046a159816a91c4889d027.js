2ff716d747229b42b678e8fdfda1e44bcff04125
KenTech-code <kennedymuriuki872@gmail.com>
2020-05-05 20:56:17 +0300

    Updates

---
+++ precache-manifest.3d8ed0224b046a159816a91c4889d027.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "474a16abc97092507bfa8a6187dabfea",
+    "url": "/pizza-delivery-app/index.html"
+  },
+  {
+    "revision": "fbd0dd3a4d6541cdede4",
+    "url": "/pizza-delivery-app/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "92586e1305ea4a1572bc",
+    "url": "/pizza-delivery-app/static/css/main.2738d85b.chunk.css"
+  },
+  {
+    "revision": "fbd0dd3a4d6541cdede4",
+    "url": "/pizza-delivery-app/static/js/2.4a94746d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pizza-delivery-app/static/js/2.4a94746d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92586e1305ea4a1572bc",
+    "url": "/pizza-delivery-app/static/js/main.4a3845a5.chunk.js"
+  },
+  {
+    "revision": "c57faf3804fb068a5d98",
+    "url": "/pizza-delivery-app/static/js/runtime-main.ff3cbd7f.js"
+  }
+]);
